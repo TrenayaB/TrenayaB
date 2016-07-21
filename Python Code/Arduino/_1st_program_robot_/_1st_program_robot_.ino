f448d9f48fd826fd4f9d75bc6cc3ddf67ce40772
TrenayaB <nayabenjamin44@gmail.com>
2016-07-21 07:01:36 -0700

    Add files via upload

---
+++ Python Code/Arduino/_1st_program_robot_/_1st_program_robot_.ino
@@ -0,0 +1,19 @@
+int LEDPIN = 4;
+int LEDPIN2 = 5;
+void setup(){
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(LEDPIN2, OUTPUT); 
+  // put your setup code here, to run once:  
+}
+
+void loop(){
+  digitalWrite(LEDPIN, HIGH);
+  delay(100);
+  digitalWrite(LEDPIN, LOW);
+  delay(100);
+  digitalWrite(LEDPIN2, LOW);
+  delay(100);
+  digitalWrite(LEDPIN2, HIGH);
+  delay(100);
+  // put your main code here, to run repeatedly:    
+}
---
+++ Python Code/Arduino/motion_robot/motion_robot.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+Servo servoRight;
+Servo servoLeft;
+
+void setup() {
+  // put your setup code here, to run once:
+servoLeft.attach(13);
+servoRight.attach(12);
+servoLeft.writeMicroseconds(1500);
+servoRight.writeMicroseconds(1500);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+servoRight.writeMicroseconds(1300);
+servoLeft.writeMicroseconds(1700); 
+servoLeft.writeMicroseconds(1500);
+servoLeft.writeMicroseconds(2000);
+servoRight.writeMicroseconds(1000);
+servoRight.writeMicroseconds(1500);
+servoLeft.writeMicroseconds(1500);
+}
